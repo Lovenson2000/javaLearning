3e374a87c822cf517355eeef82e34f39ecc8db63
Lovenson2000 <beaucicotlovenson@gmail.com>
2024-05-14 23:59:16 +0800

    Hashmap methods

--- collections/maps/hashmap/Main.java
+++ collections/maps/hashmap/Main.java
@@ -5,14 +5,27 @@ import java.util.Map;
 
 public class Main {
 
+    static <K, V> HashMap<K, V> clone(HashMap<K, V> map) {
+        return new HashMap<>(map);
+    }
+
     public static void main(String[] args) {
+        HashMap <String, Integer> people = new HashMap<>();
+        HashMap <String, Integer> otherPeople = new HashMap<>(Map.of("Wado", 26, "Jeff-G", 20, "Adidas", 30));
+        HashMap <String, Integer> copyPeople = new HashMap<>();
 
-        Map <String, Integer> people = new HashMap<>();
-        Map <String, Integer> otherPeople = new HashMap<>(Map.of("Wado", 34, "Jeff-G", 20));
-
+        // Adding elements to a hashmap
         people.put("Blatter", 23);
         people.putAll(otherPeople);
-        System.out.println(people);
+        people.putIfAbsent("Blatter", 23); // won't be added
+        copyPeople = clone(people);
+
+        // Most common methods
+        System.out.println(people.values());
+        System.out.println(people.keySet());
+        System.out.println(people.entrySet());
+        System.out.println(copyPeople.get("Wado"));
+
         
     }
 }
