155bb032c63c84004fa1f5789c7f2092650da058
Lovenson2000 <beaucicotlovenson@gmail.com>
2024-05-15 11:54:23 +0800

    more Hashmap methods

--- collections/maps/hashmap/Main.java
+++ collections/maps/hashmap/Main.java
@@ -5,27 +5,24 @@ import java.util.Map;
 
 public class Main {
 
-    static <K, V> HashMap<K, V> clone(HashMap<K, V> map) {
-        return new HashMap<>(map);
-    }
-
     public static void main(String[] args) {
         HashMap <String, Integer> people = new HashMap<>();
         HashMap <String, Integer> otherPeople = new HashMap<>(Map.of("Wado", 26, "Jeff-G", 20, "Adidas", 30));
-        HashMap <String, Integer> copyPeople = new HashMap<>();
 
         // Adding elements to a hashmap
         people.put("Blatter", 23);
-        people.putAll(otherPeople);
-        people.putIfAbsent("Blatter", 23); // won't be added
-        copyPeople = clone(people);
+        // people.putAll(otherPeople);
+        otherPeople.putIfAbsent("Blatter", 23); // won't be added
+        otherPeople.forEach((key, value) -> people.merge(key, value, (oldValue, newValue) -> oldValue + oldValue));
+        System.out.println(people);
 
         // Most common methods
         System.out.println(people.values());
         System.out.println(people.keySet());
         System.out.println(people.entrySet());
-        System.out.println(copyPeople.get("Wado"));
-
+        System.out.println(people.get("Wado"));
+        System.out.println(people.containsKey("Adidas")); // true
+        System.out.println(people.containsValue(23)); // true
         
     }
 }
